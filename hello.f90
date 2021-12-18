070c0093b5b50528277726f0918fd5cf17f51a97
MatiasBouer <matiasbower@gmail.com>
2021-12-18 17:09:31 -0300

    Agregando nombre a hello y mas numeros

--- hello.f90
+++ hello.f90
@@ -1,4 +1,7 @@
 program hello
-        write *, "Hello World"
+        character (20): myname
+        write *, "Ingrese Nombre"
+        read *, myname
+        write *, "Hello ",myname
 
 end program hello
